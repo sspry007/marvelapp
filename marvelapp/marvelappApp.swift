a816b52381fb2aa1a53d063860792c5b47f5b357
Steven Spry <sspry007@icloud.com>
2025-03-10 15:19:12 -0400

    Initial Commit

---
+++ marvelapp/marvelappApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvelappApp.swift
+//  marvelapp
+//
+//  Created by Steven Spry on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct marvelappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
